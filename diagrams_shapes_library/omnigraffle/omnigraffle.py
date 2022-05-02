62fa6dc956d068485bd376de58166c7409eeada5
Maciej Radzikowski <maciej@radzikowski.com.pl>
2022-05-02 23:06:23 +0200

    Reorder methods in OmniGraffle processor

--- diagrams_shapes_library/omnigraffle/omnigraffle.py
+++ diagrams_shapes_library/omnigraffle/omnigraffle.py
@@ -51,7 +51,13 @@ class OmniGraffle(Processor):
 
         super().process()
 
-        self._save_data_plist(self._stencil_path, self._data_pl, self._conf.text_output)
+        self._save_data_plist()
+
+    def _create_data_plist(self) -> Dict[str, Any]:
+        return self._load_plist(data_template_file)
+
+    def _load_image_plist_template(self) -> Dict[str, Any]:
+        return self._load_plist(image_template_file)
 
     def _create_dirs(self):
         super()._create_dirs()
@@ -67,22 +73,21 @@ class OmniGraffle(Processor):
             logger.debug(f'Processing file {image}')
 
             self._image_idx += 1
-            pdf_image_path = self._save_image_as_pdf(image, self._stencil_path, self._image_idx)
+            pdf_image_path = self._save_image_as_pdf(image)
             stencil_name = create_name(image, self._conf.image_name_remove)
             sheet_image_bounds.append(self._calc_next_image_bounds(pdf_image_path, sheet_image_bounds))
-            image_pl = self._create_image_plist(self._image_pl_tpl, self._image_idx, stencil_name, sheet_image_bounds[-1],
-                                                self._conf.vertex_magnets, self._conf.side_magnets)
+            image_pl = self._create_image_plist(stencil_name, sheet_image_bounds[-1])
             self._add_image_to_sheet(sheet_pl, image_pl)
 
-        self._add_sheet_to_data(self._data_pl, sheet_pl)
+        self._add_sheet_to_data(sheet_pl)
 
-    @staticmethod
-    def _add_image_to_sheet(sheet_pl: Dict[str, Any], image_pl: Dict[str, Any]) -> None:
-        sheet_pl['GraphicsList'].append(image_pl)
+    def _create_sheet_plist(self, sheet_title: str) -> Dict[str, Any]:
+        sheet_pl = self._load_plist(sheet_template_file)
+        sheet_pl['SheetTitle'] = sheet_title
+        return sheet_pl
 
-    @staticmethod
-    def _save_image_as_pdf(source: str, dir_path: str, idx: int) -> str:
-        pdf_path = os.path.join(dir_path, f'image{idx}.pdf')
+    def _save_image_as_pdf(self, source: str) -> str:
+        pdf_path = os.path.join(self._stencil_path, f'image{self._image_idx}.pdf')
         cairosvg.svg2pdf(url=source, write_to=pdf_path, dpi=72)
         return pdf_path
 
@@ -113,19 +118,19 @@ class OmniGraffle(Processor):
 
         return x, y, width, height
 
-    def _create_image_plist(self, plist_template: Dict[str, Any], idx: int, stencil_name: str, bounds: Tuple[int, int, int, int],
-                            vertex_magnets: bool, side_magnets: int) -> Dict[str, Any]:
-        image_pl = plist_template.copy()
+    def _create_image_plist(self, stencil_name: str, bounds: Tuple[int, int, int, int]) -> Dict[str, Any]:
+        image_pl = self._image_pl_tpl.copy()
+
         image_pl['Bounds'] = '{{' + str(bounds[0]) + ', ' + str(bounds[1]) + '},' + \
                              '{' + str(bounds[2]) + ', ' + str(bounds[3]) + '}}'
-        image_pl['ID'] = idx
-        image_pl['ImageID'] = idx
+        image_pl['ID'] = self._image_idx
+        image_pl['ImageID'] = self._image_idx
         image_pl['Name'] = stencil_name
 
         magnet_positions = []
-        if vertex_magnets:
+        if self._conf.vertex_magnets:
             magnet_positions.extend(self._create_vertex_magnets())
-        magnet_positions.extend(self._create_side_magnets(side_magnets))
+        magnet_positions.extend(self._create_side_magnets())
 
         magnets = ['{' + str(pos[0]) + ', ' + str(pos[1]) + '}' for pos in magnet_positions]
         image_pl['Magnets'] = magnets
@@ -141,12 +146,11 @@ class OmniGraffle(Processor):
             (-1, 1),
         ]
 
-    @staticmethod
-    def _create_side_magnets(count: int) -> List[Tuple[float, float]]:
-        factor = 2 / (count + 1)
+    def _create_side_magnets(self) -> List[Tuple[float, float]]:
+        factor = 2 / (self._conf.side_magnets + 1)
 
         magnets = []
-        for i in range(1, count + 1):
+        for i in range(1, self._conf.side_magnets + 1):
             value = -1 + factor * i
             magnets.extend([
                 (-1, value),
@@ -157,35 +161,26 @@ class OmniGraffle(Processor):
 
         return magnets
 
-    def _load_image_plist_template(self) -> Dict[str, Any]:
-        return self._load_plist(image_template_file)
-
-    def _create_data_plist(self) -> Dict[str, Any]:
-        return self._load_plist(data_template_file)
-
-    def _create_sheet_plist(self, sheet_title: str) -> Dict[str, Any]:
-        sheet_pl = self._load_plist(sheet_template_file)
-        sheet_pl['SheetTitle'] = sheet_title
-        return sheet_pl
-
     @staticmethod
-    def _add_sheet_to_data(data_pl: Dict[str, Any], sheet_pl: Dict[str, Any]) -> None:
+    def _add_image_to_sheet(sheet_pl: Dict[str, Any], image_pl: Dict[str, Any]) -> None:
+        sheet_pl['GraphicsList'].append(image_pl)
+
+    def _add_sheet_to_data(self, sheet_pl: Dict[str, Any]) -> None:
         images_count = len(sheet_pl['GraphicsList'])
 
-        data_pl['Sheets'].append(sheet_pl)
+        self._data_pl['Sheets'].append(sheet_pl)
 
-        data_pl['ImageCounter'] += images_count
-        data_pl['ImageList'].extend([f'image{image["ID"]}.pdf' for image in sheet_pl['GraphicsList']])
+        self._data_pl['ImageCounter'] += images_count
+        self._data_pl['ImageList'].extend([f'image{image["ID"]}.pdf' for image in sheet_pl['GraphicsList']])
 
     @staticmethod
     def _load_plist(file_path: str) -> Dict[str, Any]:
         with open(file_path, 'rb') as fp:
             return plistlib.load(fp)
 
-    @staticmethod
-    def _save_data_plist(path: str, data_pl: Dict[str, Any], text_output: bool) -> None:
-        data_file = os.path.join(path, 'data.plist')
+    def _save_data_plist(self) -> None:
+        data_file = os.path.join(self._stencil_path, 'data.plist')
         with open(data_file, 'wb') as fp:
-            fmt = plistlib.FMT_XML if text_output else plistlib.FMT_BINARY
+            fmt = plistlib.FMT_XML if self._conf.text_output else plistlib.FMT_BINARY
             # noinspection PyTypeChecker
-            plistlib.dump(data_pl, fp, fmt=fmt)
+            plistlib.dump(self._data_pl, fp, fmt=fmt)
